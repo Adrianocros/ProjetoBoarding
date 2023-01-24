1fb92f99c68aa8d8a1f700fcd3b3467f3ad70052
Adriano Crosoletto <=>
2023-01-24 00:28:02 -0300

    Ajuste no Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/styles.module.scss";
 
+
 export default function Home() {
   return (
     <>
@@ -8,19 +9,18 @@ export default function Home() {
       <title>Boarding - Organizize-se</title>
     </Head>
     <main className={styles.contentConteiner}>
-        <img src="/images/board-user.svg" alt="ferramenta board" />
+        <img src="/images/board-user.svg" alt="ferramenta boarding" />
       
       <section className={styles.callToAction}>
-        <h1>Feramenta para seu dia a dia ser organizado e se plenejado...</h1>
+        <h1>Feramenta para seu dia a dia ser mais plenejado e organizado...</h1>
+        
         <p>
           <span>100% Gratis </span> e on-line.
           </p>
       </section>
       <p className={styles.apoadores}>Apoiadores:</p>
       <div className={styles.donaters}>
-        
         <img src="https://sujeitoprogramador.com/steve.png" alt="Colaborador 1" />
-          
       </div>
     </main>
     </>
